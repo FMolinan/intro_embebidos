b9d55840a74755a6a7ffe4fd0673db76cee8a5dd
Felipe Molina <felipe.molina@wisely.cl>
2019-05-15 20:16:00 +0100

    refactoring and cleaning

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify, render_template, request
+from config import Config
 import RPi.GPIO as io
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 LED_CHANNEL = 7
 io.setmode(io.BOARD)
 io.setup(LED_CHANNEL, io.OUT)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
     if request.method == 'POST':
+        print("CLICK!")
         if io.input(LED_CHANNEL):
-            io.ouput(LED_CHANNEL, False)
+            io.output(LED_CHANNEL, False)
         else:
             io.output(LED_CHANNEL, True)
+        return render_template('home.html', project='Ejemplo GPIO')
     elif request.method == 'GET':
         return render_template('home.html', project='Ejemplo GPIO')
